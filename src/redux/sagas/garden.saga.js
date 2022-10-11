9bb49ee7eb07252a3fd520bd398c4d868f0a5974
Sarah Preston <seprest@gmail.com>
2022-10-11 16:41:02 -0500

    added saga functions

--- src/redux/sagas/garden.saga.js
+++ src/redux/sagas/garden.saga.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function* gardenSaga() {
     yield takeLatest('FETCH_PLANTS', fetchPlants);
+    yield takeLatest('SEND_PLOT', addPlot);
   }
   
 function* fetchPlants(){
@@ -13,6 +14,21 @@ function* fetchPlants(){
     }                       //set plant reducer to list of plants
     catch(error){
         console.log('fetchPlants failed', error);
+    };
+};
+
+function* addPlot(action){
+    try{
+        console.log('Plot being added', action.payload, action.month);
+        const plot = yield axios({
+            method: 'POST',
+            url: '/garden/add_plot', 
+            data: {plot: action.payload, month: action.month}
+        });
+        yield PUT({ type: 'CLEAR_PLOT' });
+    }
+    catch(error){
+        console.log('addPlot failed,', error);
     };
 };
 
