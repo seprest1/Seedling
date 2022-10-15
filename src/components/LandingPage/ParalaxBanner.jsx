55d84729335d746f78d7de095710d8eaa0912689
Sarah Preston <seprest@gmail.com>
2022-10-15 10:56:13 -0500

    added paralax banner

---
+++ src/components/LandingPage/ParalaxBanner.jsx
@@ -0,0 +1,46 @@
+import './LandingPage.css';
+import { ParallaxBanner, ParallaxBannerLayer } from 'react-scroll-parallax';
+import { useParallax } from 'react-scroll-parallax';
+
+function ParalaxBanner(){
+    const background = {
+        image:
+          'Images/Background.jpeg',
+        speed: -40,
+        translateY: [0, 50],
+        shouldAlwaysCompleteAnimation: true,
+      };
+    
+      const middleground = {
+        image:
+          'Images/middleground.png',
+        speed: -20,
+        shouldAlwaysCompleteAnimation: true,
+      };
+    
+      const foreground = {
+        image:
+          'Images/foreground.png',
+        speed: 20,
+        translateY: [0, 15],
+        scale: [1, 1.1, 'easeOutCubic'],
+        shouldAlwaysCompleteAnimation: true,
+      };
+    
+      const headline = {
+        scale: [1, 1.05, 'easeOutCubic'],
+        expanded: false,
+        children: (
+            <h1 className="welcome_title">Welcome</h1>
+        ),
+      };
+
+    return(
+        <ParallaxBanner
+            layers={[background, middleground, foreground, headline]}
+            className="paralax"
+          />
+    )
+}
+
+export default ParalaxBanner
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,7 +1,6 @@
 import '../App.css';
 import './UserPage.css';
 //components
-import PlotDisplay from './PlotDisplay';
 import WelcomeDisplay from './WelcomeDisplay';
 
 function UserPage() {
@@ -10,9 +9,7 @@ function UserPage() {
     <div className="user_body">
       <div className="user_welcome_display">
         <WelcomeDisplay/>
-      </div>
-      <div className="user_plot_display">
-        <PlotDisplay/>
+        
       </div>
     </div>
   );
