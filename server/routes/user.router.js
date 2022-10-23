497bca3413452347e349bd38c345279cb968b774
Sarah Preston <seprest@gmail.com>
2022-10-23 09:59:38 -0500

    set up more architecture for weather API

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {
-  rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+const api_key = process.env.ACCUWEATHER_API_KEY;
 
 // Handles Ajax request for user information if user is authenticated
 router.get('/', rejectUnauthenticated, (req, res) => {
@@ -15,36 +15,36 @@ router.get('/', rejectUnauthenticated, (req, res) => {
 });
 
 // Handles POST request with new user data
-// The only thing different from this and every other post we've seen
-// is that the password gets encrypted before being inserted
-router.post('/register', (req, res, next) => {
-  console.log(req.body);
-  const username = req.body.username;
-  const password = encryptLib.encryptPassword(req.body.password);
-  const zipcode = req.body.zipcode;
+router.post('/register', async (req, res, next) => {
+  try{
+    console.log(req.body);
+    const username = req.body.username;
+    const password = encryptLib.encryptPassword(req.body.password);
+    const zipcode = req.body.zipcode;
 
-  const queryText = `INSERT INTO "user" (username, password, zipcode)
-    VALUES ($1, $2, $3) RETURNING id`;
-  pool
-    .query(queryText, [username, password, zipcode])
-    .then(() => res.sendStatus(201))
-    .catch((err) => {
-      console.log('User registration failed: ', err);
-      res.sendStatus(500);
-    });
+    //gets weather key from accuweather API
+    const response = await axios.get(`http://dataservice.accuweather.com/locations/v1/postalcodes/search?apikey=${api_key}&q=${zipcode}&language=en-us`);
+    const weather_key = response.data[0].Key;
+    console.log(weather_key);
+
+    const queryText = `INSERT INTO "user" (username, password, zipcode, weather_key)
+      VALUES ($1, $2, $3, $4) RETURNING id`;
+    await pool.query(queryText, [username, password, zipcode, weather_key])
+    res.sendStatus(201)
+  }
+  catch(error){
+    console.log('User registration failed: ', err);
+    res.sendStatus(500);
+  };
 });
 
 // Handles login form authenticate/login POST
-// userStrategy.authenticate('local') is middleware that we run on this route
-// this middleware will run our POST if successful
-// this middleware will send a 404 if not successful
 router.post('/login', userStrategy.authenticate('local'), (req, res) => {
   res.sendStatus(200);
 });
 
 // clear all server session information about this user
 router.post('/logout', (req, res) => {
-  // Use passport's built-in method to log out the user
   req.logout();
   res.sendStatus(200);
 });
